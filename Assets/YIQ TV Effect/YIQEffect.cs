2e6f6dc8f1a0754e710f7ccfab32eaee0dab3c1c
Ivan Notaros <nothke@gmail.com>
2016-11-11 00:24:11 +0100

    Added YIQ tv effect

---
+++ Assets/YIQ TV Effect/YIQEffect.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+public class YIQEffect : ImageEffectBase
+{
+    public AnimationCurve yCurve;
+    public AnimationCurve iqCurve;
+    public float distance = 1;
+
+    void SetCurves()
+    {
+        /*
+        var materialProperty = new MaterialPropertyBlock();
+
+        materialProperty.SetFloatArray("arrayName", floatArray);
+        gameObject.GetComponent<Renderer>().SetPropertyBlock(materialProperty);
+
+        material.set*/
+
+        float[] yValues = new float[10];
+        float[] iqValues = new float[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            yValues[i] = yCurve.Evaluate((float)i / 10);
+            iqValues[i] = iqCurve.Evaluate((float)i / 10);
+        }
+
+        //material.SetFloatArray("yCurve", yValues);
+        //material.SetFloatArray("iqCurve", iqValues);
+
+        Shader.SetGlobalFloatArray("_YCurve", yValues);
+        Shader.SetGlobalFloatArray("_IQCurve", iqValues);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetCurves();
+
+        material.SetFloat("_Distance", distance);
+
+        Graphics.Blit(source, destination, material);
+    }
+}
