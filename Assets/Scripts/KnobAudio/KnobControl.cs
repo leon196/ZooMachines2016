3ae08f522c103c44fc68cdb848604b3b83816fb7
Ivan Notaros <nothke@gmail.com>
2016-11-11 00:26:21 +0100

    Added knob key contol scripts and clips

---
+++ Assets/Scripts/KnobAudio/KnobControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using MidiJack;
+
+public class KnobControl : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    [System.Serializable]
+    public class KnobProp
+    {
+        new public string name;
+        public float min = 0;
+        public float max = 1;
+    }
+
+    public KnobProp[] knobProps;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < knobProps.Length; i++)
+        {
+            ProcessSound(i, knobProps[i].name, knobProps[i].min, knobProps[i].max);
+        }
+    }
+
+    void ProcessSound(int slider, string propertyName, float min, float max)
+    {
+
+        if (string.IsNullOrEmpty(propertyName)) return;
+
+        float value = Mathf.Lerp(min, max, MidiMaster.GetKnob(slider));
+        mixer.SetFloat(propertyName, value);
+    }
+}
---
+++ Assets/Scripts/KnobAudio/PitchBend.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using MidiJack;
+using UnityEngine.Audio;
+
+public class PitchBend : MonoBehaviour
+{
+
+    public AudioClip c;
+
+    public SfxrSynth synth;
+    public SfxrParams param;
+
+    public AudioMixerGroup group;
+
+    void Update()
+    {
+        for (int i = 0; i < 128; i++)
+        {
+            if (!MidiMaster.GetKeyDown(i))
+            {
+                continue;
+            }
+
+            //Debug.Log("Pressed " + i);
+
+            float value = MidiMaster.GetKey(i);
+            value = 1;
+            float pitch = Mathf.Pow(2, (i - 48) / 12f);
+
+            AudioSource src = c.PlayOnce(Vector3.zero, value, pitch, 90, 10001);
+            src.outputAudioMixerGroup = group;
+        }
+    }
+}
